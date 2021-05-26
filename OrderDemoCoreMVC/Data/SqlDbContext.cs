4d5dd12df9f2596aca6f8960b5c2bc3f4a5511d9
Marek Stýblo <marek.styblo@aftg.cz>
2021-05-26 17:29:52 +0200

    rename tables to plural

--- OrderDemoCoreMVC/Data/SqlDbContext.cs
+++ OrderDemoCoreMVC/Data/SqlDbContext.cs
@@ -12,9 +12,9 @@ namespace OrderDemoCoreMVC.Data
         {
         }
 
-        public DbSet<Product> Product { get; set; }
-        public DbSet<OrderItem> OrderItem { get; set; }
-        public DbSet<Order> Order { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
 
 
 
