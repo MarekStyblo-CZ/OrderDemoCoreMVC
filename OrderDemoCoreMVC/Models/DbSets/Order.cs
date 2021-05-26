68033bf2bda40fee539496b71034bf3295443b87
Marek Stýblo <marek.styblo@aftg.cz>
2021-05-26 15:31:09 +0200

    db models

---
+++ OrderDemoCoreMVC/Models/DbSets/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrderDemoCoreMVC.Models.DbSets
+{
+    public class Order
+    {
+        //PK
+        [Key, Column(Order = 0), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        //Relations
+        public List<OrderItem> OderItem { get; set; }
+
+        //Attr
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
---
+++ OrderDemoCoreMVC/Models/DbSets/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrderDemoCoreMVC.Models.DbSets
+{
+    public class Product
+    {
+        //PK
+        [Key, Column(Order = 0), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        //Relations
+        public List<OrderItem> OrderItems { get; set; }
+
+        //Attr
+
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+
+    }
+}
