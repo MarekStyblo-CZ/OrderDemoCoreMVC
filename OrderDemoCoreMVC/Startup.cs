7d8d6b9cec925f265d96dccb3fbc4f8aa7eb9f4c
Marek Stýblo <marek.styblo@aftg.cz>
2021-05-26 17:09:00 +0200

    setup entity framework

--- OrderDemoCoreMVC/Startup.cs
+++ OrderDemoCoreMVC/Startup.cs
@@ -1,12 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using OrderDemoCoreMVC.Data;
 
 namespace OrderDemoCoreMVC
 {
@@ -22,6 +20,11 @@ namespace OrderDemoCoreMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SqlDbContext>(options =>
+               options.UseSqlServer(
+                   Configuration.GetConnectionString("DbConnection")));
+
+
             services.AddControllersWithViews();
         }
 
